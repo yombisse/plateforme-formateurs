6e3064990abd954ff2502fb25591372416e194db
Fandie-yombisse <yombissefandie@gmail.com>
2026-08-31 14:51:25 +0000

    fix: use faker property in UserFactory

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,24 +3,25 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
-    use WithoutModelEvents;
-
     /**
      * Seed the application's database.
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Formateur Test',
-            'email' => 'formateur@test.local',
-            'password' => 'password',
-        ]);
+        User::updateOrCreate(
+            [
+                'email' => 'formateur@test.local',
+            ],
+            [
+                'name' => 'Formateur Test',
+                'password' => Hash::make('password'),
+                'email_verified_at' => now(),
+            ]
+        );
     }
 }
